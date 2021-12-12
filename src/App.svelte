1f16dfdca48ab1a1d4364b1b3bb02b4d6551687e
Melissa Fernandes <87154023+mellunar@users.noreply.github.com>
2021-12-12 12:17:11 -0300

    Update App.svelte

--- src/App.svelte
+++ src/App.svelte
@@ -3,14 +3,14 @@
 	import LanguageRadio from "./components/language-radio.svelte";
 </script>
 
-<style lang="scss">
+<style global lang="scss">
 	@import './app.scss';
 </style>
 
 <main>
 	<h1>{$t('title')}</h1>
 	<p>{$t('msg1')}<a href="https://svelte.dev/tutorial">{$t('msg2')}</a>{$t('msg3')}</p>
-	<div global class="options">
+	<div class="options">
 		<LanguageRadio iso='en' />
 		<LanguageRadio iso='pt' class="radio" />
 	</div>
